66752d0e5adab6564db12718b59befad079c2ad3
Rj12-art <rashj40@gmail.com>
2021-07-31 13:08:26 +0000

    added a data-type to the card elements

---
+++ assets/index.js
@@ -0,0 +1,29 @@
+
+document.addEventListener("DOMContentLoaded",function(){
+    let cards = document.getElementsByClassName("card");
+
+    for (let card of cards){
+       card.addEventListener("click",function() {
+           if (this.getAttribute(""))
+       })
+    };
+})
+
+
+function runGame() {
+
+}
+
+function runTime() {
+
+}
+
+
+function checkCard() {
+
+}
+
+function incrementScore(){
+
+}
+
